fdf9c78b2e9d8a5d91bad1960facebbffae7fb4e
John Servo <jaservo@up.edu.ph>
2022-06-24 01:46:03 +0800

    added base design for sidebar will edit checkbox later

--- scripts/script.js
+++ scripts/script.js
@@ -5,10 +5,12 @@ const close = document.getElementById("close");
 
 addBookButton.addEventListener('click', ()=> {
     sidebar.style.width = "20vw";
+    sidebar.style.padding = "1rem"
 });
 
 close.addEventListener('click', () => {
     sidebar.style.width = "0";
+    sidebar.style.padding = "0";
 });
 
 
