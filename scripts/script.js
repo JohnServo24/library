d2912f4a11ce7b8b3c867707d6086aac1debf68c
John Servo <jaservo@up.edu.ph>
2022-06-24 00:59:34 +0800

    added skeleton for sidebar

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,15 @@
 
 const sidebar = document.getElementById("sidebar");
+const addBookButton = document.getElementById("addBookButton");
+const close = document.getElementById("close");
+
+addBookButton.addEventListener('click', ()=> {
+    sidebar.style.width = "20vw";
+});
+
+close.addEventListener('click', () => {
+    sidebar.style.width = "0";
+});
 
 
 
