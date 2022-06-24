5331f1bbbae1ea43a74242e1be1314d16f1531e5
John Servo <jaservo@up.edu.ph>
2022-06-24 11:41:51 +0800

    added blur effect

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,21 @@
 
 const sidebar = document.getElementById("sidebar");
+const main = document.getElementById("main");
 const addBookButton = document.getElementById("addBookButton");
 const close = document.getElementById("close");
 
 addBookButton.addEventListener('click', ()=> {
-    sidebar.style.width = "20vw";
+    sidebar.style.width = "400px";
     sidebar.style.padding = "1rem"
+    main.style.transition = "1s";
+    main.style.filter = "blur(5px)";
 });
 
 close.addEventListener('click', () => {
     sidebar.style.width = "0";
     sidebar.style.padding = "0";
+    main.style.transition = "0";
+    main.style.filter = "blur(0)";
 });
 
 
