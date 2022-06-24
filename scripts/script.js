f1aeda2e8bb839fb161dad54950843c011bad471
John Servo <jaservo@up.edu.ph>
2022-06-24 18:56:29 +0800

    Some changes:
    > Turned the sidebar transitions into functions
    > Added function to number the IDs of the books/cards
    > The querySelector for the checkbox and the delete button
        now updates

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,10 @@
 // Traverse through the list finding that title
 // Toggle the read to either true or false
 
+// -- If user wants to delete
+// Listen to the delete button
+// If clicked, delete the book where the delete button is in
+
 
 let myLibrary = [];
 
@@ -27,6 +31,13 @@ const addBookToLibrary = (title, author, pages, read) => {
 const addToPage = (title, author, pages, read) => {
     const grid = document.getElementById("grid");
 
+    // Identifies the position of the card in the library
+    // and uses it as the class for the card 
+    let cardNo = 0;
+    myLibrary.forEach((item, i) => {
+        if(item.title == title) cardNo = i;
+    });
+
     // Creates the card
     const card = document.createElement("div");
     const text = document.createElement("div");
@@ -39,6 +50,7 @@ const addToPage = (title, author, pages, read) => {
 
     // Adds the classes to card
     card.classList.add("card");
+    card.setAttribute("id", `card-${cardNo}`);
     text.classList.add("text");
     icons.classList.add("icons");
     checkbox.setAttribute("type", "checkbox");
@@ -60,6 +72,20 @@ const addToPage = (title, author, pages, read) => {
     card.append(icons);
     icons.append(checkbox);
     icons.append(deleteButton);
+}
+
+const clearSidebar = () => {
+    sidebar.style.width = "0";
+    sidebar.style.padding = "0";
+    main.style.transition = "0";
+    main.style.filter = "blur(0)";
+}
+
+const openSidebar = () => {
+    sidebar.style.width = "400px";
+    sidebar.style.padding = "1rem"
+    main.style.transition = "1s";
+    main.style.filter = "blur(5px)";
 }
 
 // Clear all items, to be used with displayAll if we want to display something
@@ -85,8 +111,6 @@ const author = document.getElementById("author");
 const pages = document.getElementById("pages");
 const checkbox = document.getElementById("checkbox");
 
-// Selects all checkboxes
-const cardCheckbox = main.querySelectorAll("input[type=checkbox]");
 
 // Sample books
 addBookToLibrary("The Great Gatsby", "F. Scott FItzgerald", "1", true);
@@ -94,20 +118,30 @@ addBookToLibrary("1984", "George Orwell", "2", false);
 addBookToLibrary("To Kill a Mockingbird", "Harper Lee", "2", true);
 displayAll(myLibrary);
 
+// Selects all checkboxes
+let cardCheckbox = main.querySelectorAll("input[type=checkbox]");
+
+// Selects all delete icons
+let deleteIcon = main.querySelectorAll("span[class=material-symbols-outlined]");
+
 // Form to add new books
 form.addEventListener("submit", () => {
     addBookToLibrary(title.value, author.value, pages.value, read.checked);
     addToPage(title.value, author.value, pages.value, read.checked);
 
-    sidebar.style.width = "0";
-    sidebar.style.padding = "0";
-    main.style.transition = "0";
-    main.style.filter = "blur(0)";
+    clearSidebar();
+    
 
+    // Clears the values
     title.value = "";
     author.value = "";
     pages.value = "";
     read.checked = false;
+
+    // Updates the checkbox and delete icon node list
+    cardCheckbox = main.querySelectorAll("input[type=checkbox]");
+    deleteIcon = main.querySelectorAll("span[class=material-symbols-outlined]");
+
 });
 
 // Toggles the checkbox on the library
@@ -133,24 +167,17 @@ cardCheckbox.forEach(item => {
     });
 });
 
+deleteIcon.forEach(item => {
+    item.addEventListener("click", () => {
+        console.log("yeah");
+    });
+});
+
 // Page effects
 
-addBookButton.addEventListener('click', () => {
-    sidebar.style.width = "400px";
-    sidebar.style.padding = "1rem"
-    main.style.transition = "1s";
-    main.style.filter = "blur(5px)";
-});
+addBookButton.addEventListener('click', openSidebar);
 
-close.addEventListener('click', () => {
-    sidebar.style.width = "0";
-    sidebar.style.padding = "0";
-    main.style.transition = "0";
-    main.style.filter = "blur(0)";
-});
-
-
-// test.parentElement.parentElement.querySelector("#testtest").firstElementChild.textContent
+close.addEventListener('click', clearSidebar);
 
 
 
