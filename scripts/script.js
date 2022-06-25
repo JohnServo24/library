df598c2340b811fe78ef9f1da08600bfb01d2bb6
John Servo <jaservo@up.edu.ph>
2022-06-25 13:18:23 +0800

    edited readme

--- scripts/script.js
+++ scripts/script.js
@@ -15,13 +15,6 @@
     // Traverse the database for the book title
     // If book title matches, delete it
 
-//--- TO ADD:
-// > Checks if the book exists (WILL ADD LATER BECAUSE I HAVENT LEARNED ASYNC/PROMISES YET)
-// > Store book locally (WILL DO LATER BECAUSE I HAVENT LEARNED JSON YET)
-// > Add a unique identification for each book since if we have two books of the
-// same name but w/ different authors and we delete one of them, it deletes
-// every book in the library with the same name regardless of the author
-
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
